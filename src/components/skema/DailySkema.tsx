4fd9f43a516036f05f5411380d6b8cbf1474836b
pete589d <pete589d@zbc.dk>
2021-01-23 12:18:43 +0100

    added horizontal paging for timetable

--- src/components/skema/DailySkema.tsx
+++ src/components/skema/DailySkema.tsx
@@ -1,10 +1,11 @@
 import { inject, observer } from "mobx-react";
 import React, { Component } from "react";
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, Dimensions } from "react-native";
 import ThemeStore from "../../stores/ThemeStore";
 import { Lesson } from "liblectio/lib/Skema/Timetable"
 import SkemaBrik from "../skema/SkemaBrik"
 
+const { width } = Dimensions.get('window');
 
 interface DailySkemaProps {
     lessons: Lesson[];
@@ -18,7 +19,7 @@ export default class DailySkema extends Component<DailySkemaProps>{
         console.log(JSON.stringify(this.props.lessons, null, 4));
         return (
             <View
-                style={{ flex: 1, flexDirection: "column", alignItems: "stretch", padding: 10 }}>
+                style={{ flex: 1, flexDirection: "column", alignItems: "stretch", padding: 10, width: width }}>
                 { this.props.lessons.map((lesson, i) => {
                     let pauseDuration = 0;
                     if (i + 1 < this.props.lessons.length) {
--- src/screens/SkemaScreen.tsx
+++ src/screens/SkemaScreen.tsx
@@ -1,12 +1,14 @@
 import { observable } from "mobx";
 import { inject, observer } from "mobx-react";
 import React, { Component } from "react";
-import { Button, ScrollView, StyleSheet, View, Text } from "react-native";
+import { Button, ScrollView, StyleSheet, View, Text, Dimensions } from "react-native";
 import { PanGestureHandlerGestureEvent } from "react-native-gesture-handler";
 import DailySkema from "../components/skema/DailySkema"
 import Lesson from "liblectio/lib/Skema/Timetable"
 import LectioStore from "../stores/LectioStore";
 import ThemeStore from "../stores/ThemeStore";
+
+const { width } = Dimensions.get('window');
 
 interface SkemaScreenProps {
   lectio: LectioStore;
@@ -20,26 +22,32 @@ export class SkemaScreen extends Component<SkemaScreenProps> {
   @observable items: JSX.Element[] = [];
   @observable weekSkema: Lesson.Lesson[][] = new Array<Array<Lesson.Lesson>>();
 
+
   async componentDidMount() {
     //console.log(this.props.lectio.password)
     // Here we should fetch the timetable
     let now = new Date();
     let onejan = new Date(now.getFullYear(), 0, 1);
-    let week = 3;//Math.ceil((((now.getTime() - onejan.getTime()) / 86400000) + onejan.getDay() + 1) / 7) - 1;
-    try {
-      await this.props.lectio.GetBriefLessonList(2021, week);
+    let week = 4; //Math.ceil((((now.getTime() - onejan.getTime()) / 86400000) + onejan.getDay() + 1) / 7) - 1;
 
-  } catch (error) {
+    try {
+      await this.props.lectio.GetBriefLessonList(now.getFullYear(), week);
+    } catch (error) {
       alert("ERROR: " + error)
     }
-    
+
+    console.log(JSON.stringify(this.props.lectio.lessonList, null, 4));
+
     // Here we split the timetable into the 7 days of the week
     let currentDay = this.props.lectio.lessonList[0].start.getDate();
     let currentDaySkema: Lesson.Lesson[] = [];
-    
-    for (let lesson of this.props.lectio.lessonList) {
 
-    if (lesson.start.getDate() == currentDay) {
+    this.props.lectio.lessonList.map((lesson, i) => {
+      if (i == this.props.lectio.lessonList.length - 1) {
+        this.weekSkema.push([...currentDaySkema]);
+      }
+
+      if (lesson.start.getDate() == currentDay) {
         currentDaySkema.push(lesson)
       }
       else {
@@ -47,31 +55,43 @@ export class SkemaScreen extends Component<SkemaScreenProps> {
         currentDaySkema = [lesson];
         currentDay = lesson.start.getDate();
       }
-    }
+    })
+
 
     //console.log(JSON.stringify(this.weekSkema[2], null, 4));
 
   }
-
-
-
   render() {
-
     return (
 
-      //<ScrollView horizontal={true} pagingEnabled={true} showsVerticalScrollIndicator={false}
-      //  showsHorizontalScrollIndicator={false} style={{flex: 1}}>
-      <>
-        {this.weekSkema.length < 4 ? <></> :
-          <DailySkema lessons={this.weekSkema[2]}>
-
-          </DailySkema>
+      <ScrollView
+        style={{}}
+        //pagingEnabled={true}
+        showsHorizontalScrollIndicator={false}
+        horizontal={true}
+        decelerationRate={0}
+        snapToInterval={width}
+        snapToAlignment={"center"}
+        contentInset={{
+          top: 0,
+          left: 30,
+          bottom: 0,
+          right: 30,
+        }}>
+        {this.props.lectio.lessonList.length < 1 ? <></> :
+          <>
+            {this.weekSkema.length < /*tal som er antal dage i skemaugen*/ this.props.lectio.lessonList[this.props.lectio.lessonList.length - 1].stop.getDate() - this.props.lectio.lessonList[0].start.getDate() + 1
+              ? <></> :
+              <>
+                {this.weekSkema.map((lessons, i) => {
+                  return <DailySkema lessons={lessons} />
+                })}
+              </>
+            }
+          </>
         }
 
-
-
-      </>
-      //</ScrollView>
+      </ScrollView>
     )
   }
 
