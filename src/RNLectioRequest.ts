119ad60a08934afdffdfffe0c676dcfac24d1f72
Mathias Gredal <mathiasgredal@icloud.com>
2021-02-11 12:55:30 +0100

    Add definition in RNLectioRequest.ts

--- src/RNLectioRequest.ts
+++ src/RNLectioRequest.ts
@@ -54,4 +54,19 @@ export class RNRequest extends LectioRequest {
       );
     });
   }
+
+  async DownloadLectio(url: string): Promise<LectioResponse> {
+    return new Promise(async (resolve, reject) => {
+      RNLectioRequest.DownloadLectio(
+        url,
+        (data: string, headers: Map<string, string>, error: string) => {
+          if (error !== 'success') {
+            reject(error);
+          }
+          // console.log(data);
+          resolve({data: data, headers: headers});
+        },
+      );
+    });
+  }
 }
